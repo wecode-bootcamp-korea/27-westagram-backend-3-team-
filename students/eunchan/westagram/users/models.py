609f3a278227c586317cfcb22ea6f7d90ebd7b90
Daco <dacokim32@gmail.com>
2021-11-18 18:55:36 +0900

    models commit_#3

--- students/eunchan/westagram/users/models.py
+++ students/eunchan/westagram/users/models.py
@@ -1,14 +1,11 @@
 from django.db import models
 
-# Create your models here.
-
-
 class Member(models.Model):
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=200)
-    phone_num = models.CharField(max_length=20)
-    person_info = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=20)
+    information = models.CharField(max_length=200, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
