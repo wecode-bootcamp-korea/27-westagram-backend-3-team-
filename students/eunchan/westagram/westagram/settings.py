008a4f2a138cb86171acb19f41520177d35779db
Daco <dacokim32@gmail.com>
2021-11-18 16:27:48 +0900

    second commit

--- students/eunchan/westagram/westagram/settings.py
+++ students/eunchan/westagram/westagram/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from my_settings    import DATABASES, SECRET_KEY
+
 import pymysql
 
+from my_settings    import DATABASES, SECRET_KEY
+
+
+# m1 맥북
 pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = SECRET_KEY 
+DATABASES = DATABASES 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,7 +85,7 @@ WSGI_APPLICATION = 'westagram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = DATABASES 
+
 
 
 
