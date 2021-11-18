5caec4dd47cb13500286f972cb6e6401bf83d186
lilo <eunehghim@gmail.com>
2021-11-18 19:06:49 +0900

    westagram_setting_modeling_2nd

--- students/eunhyekim/users/models.py
+++ students/eunhyekim/users/models.py
@@ -1,15 +1,13 @@
 from django.db                      import models
-from django.db.models.deletion      import CASCADE
-from django.db.models.lookups       import IntegerFieldFloatRounding
+
 
 class User(models.Model):
-    name     = models.CharField(max_length = 45)
-    email    = models.CharField(max_length = 45)
-    password = models.CharField(max_length = 45)
-    contact  = models.CharField(max_length = 45)
+    name       = models.CharField(max_length = 45)
+    email      = models.CharField(max_length = 200, unique = True)
+    password   = models.CharField(max_length = 200)
+    contact    = models.CharField(max_length = 200)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateField(auto_now = True)
     
     class Meta:
         db_table = 'users'
-
-
-
