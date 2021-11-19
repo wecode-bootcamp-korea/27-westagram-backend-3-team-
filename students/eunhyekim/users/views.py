c55fae9a16f17d1986538fe6265d69e58a988614
lilo <eunehghim@gmail.com>
2021-11-19 18:47:51 +0900

    westagram_setting_sign_up

--- students/eunhyekim/users/views.py
+++ students/eunhyekim/users/views.py
@@ -7,7 +7,29 @@ from django.views     import View
 from users.models     import User
 from django.db        import IntegrityError
 
+class log_in(View):
+    def Post(self, request):
+        try:
+            data = json.loads(request.body)
     
+            user_list     = User.objects.all()
+            password_list = User.objects.all()
+
+            email    = data["email"]
+            password = data["password"]
+
+            if email not in user_list:
+                return JsonResponse({"MESSAGE":"INVALID_USER"}, status = 401) 
+            elif password not in password_list: 
+                return JsonResponse({"MESSAGE":"INVALID_USER"}, status = 401)
+
+        except KeyError:
+            return JsonResponse({"MESSAGE":"KEY_ERROR"}, status = 400)
+        
+        return JsonResponse({"MESSAGE" : "SUCCESS"}, status = 201)
+
+        
+
 class UsersView(View):
     def post(self, request):
       
@@ -20,7 +42,7 @@ class UsersView(View):
             password_validation = re.compile('(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%*^&+=])([a-zA-Z0-9!@#$%*^&+=]{8,})')
             
             if not email_validation.match(email):
-                raise KeyError
+                return JsonResponse({},) #함수니까, return이 되면, 함수가 종료
             
             if not password_validation.match(password):
                 raise KeyError
