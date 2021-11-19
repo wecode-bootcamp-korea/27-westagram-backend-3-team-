8661c9e148c82214c7dfe096d7014152ba29a804
lilo <eunehghim@gmail.com>
2021-11-19 18:58:24 +0900

    westagram_setting_sign_up_second

--- students/eunhyekim/users/views.py
+++ students/eunhyekim/users/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+import json
+import re
 
-# Create your views here.
+from django.http      import JsonResponse
+from django.views     import View
+
+from users.models     import User
+from django.db        import IntegrityError
+
+        
+
+class SignUpView(View):
+    def post(self, request):
+      
+        try:
+            data       = json.loads(request.body)
+            email      = data["email"]
+            password   = data["password"]
+
+            email_validation    = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+            password_validation = re.compile('(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%*^&+=])([a-zA-Z0-9!@#$%*^&+=]{8,})')
+            
+            if not email_validation.match(email):
+                return JsonResponse({},) #함수니까, return이 되면, 함수가 종료
+            
+            if not password_validation.match(password):
+                raise KeyError
+                
+            User.objects.create(
+                name     = data["name"],
+                email    = data["email"],
+                password = data["password"],
+                contact  = data["contact"],
+            )
+
+            return JsonResponse({"MESSAGE" : "SUCCESS"}, status = 201)
+        
+        except IntegrityError:
+            return JsonResponse({"user_MESSAGE":"user_ERROR"}, status = 400)
+
+        except KeyError:
+            return JsonResponse({"MESSAGE":"KEY_ERROR"}, status = 400)
+       
+        
+       
+
