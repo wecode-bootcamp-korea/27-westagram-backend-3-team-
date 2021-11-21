2eb95e5b27a9bd9ab57866debcb3ca03cf91352e
Daco <dacokim32@gmail.com>
2021-11-21 14:54:34 +0900

    mission3 commit_#1

--- students/eunchan/westagram/users/models.py
+++ students/eunchan/westagram/users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+
 class Member(models.Model):
     name          = models.CharField(max_length=50)
     email         = models.CharField(max_length=100, unique=True)
     password      = models.CharField(max_length=200)
     phone_number  = models.CharField(max_length=20)
-    information   = models.CharField(max_length=200, blank=True, null=True)
+    information   = models.CharField(max_length=200, default="", blank=True)
     created_at    = models.DateTimeField(auto_now_add=True)
     updated_at    = models.DateTimeField(auto_now=True)
 
--- students/eunchan/westagram/users/views.py
+++ students/eunchan/westagram/users/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+import json, re
 
-# Create your views here.
+from django.http            import JsonResponse
+from django.views           import View
+from django.core.exceptions import ValidationError
+
+from users.models           import Member
+
+
+email_regexp    = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+password_regexp = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&(){}\[\]])[A-Za-z\d@$!%*#?&(){}\[\]]{8,}$"
+
+class SignupView(View):
+    def post(self, request):
+
+        try:
+            data     = json.loads(request.body) 
+            email    = data["email"]
+            password = data["password"]
+
+            if not re.match(email_regexp, email): 
+                return JsonResponse({'massage':"EMAIL_ERROR"}, status=400)
+
+            elif not re.match(password_regexp , password):
+                return JsonResponse({'massage':"PASSWORD_ERROR"}, status=400)
+
+            else : 
+                member = Member(
+                    name         = data["name"],
+                    email        = email,
+                    password     = password,
+                    phone_number = data["phone_number"],
+                    information  = data["information"]
+                )
+                member.full_clean() 
+                member.save()
+                return JsonResponse({'massage':"SUCCESS"}, status=201)
+
+        except ValidationError as e:
+            return JsonResponse({'massage':"VALIDATION_ERROR"+" "+str(e)}, status=400)
+
+        except KeyError as e:
+            return JsonResponse({'massage':"KEY_ERROR"+" Missing"+str(e)}, status=400)
+
+
+
