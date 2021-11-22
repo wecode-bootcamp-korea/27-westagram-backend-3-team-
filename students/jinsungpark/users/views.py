52a354f8096b5d711cf0675a5b3146127b985c33
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2021-11-22 16:01:10 +0900

    회원가입 수정본10

--- students/jinsungpark/users/views.py
+++ students/jinsungpark/users/views.py
@@ -1,40 +1,38 @@
-from django.core.exceptions import ValidationError
+import re, json, bcrypt
 
 from django.http.response   import JsonResponse
-
-import re
-import json
-
 from django.views           import View
+
 from .models                import User
 
 class SignUpView(View):
     def post(self, request):
         try:
             data         = json.loads(request.body)
+            email        = data['email']
+            passwd       = data["password"]
             email_regex  = '^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$'
             passwd_regex = '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$'
 
-            if re.match(email_regex, data["email"]) is None:
+            if User.objects.filter(email=email).exists():
+                return JsonResponse({"message" : "USER_ALREADY_EXISTS"}, status=400)
+
+            if not re.match(email_regex, email):
                 return JsonResponse({"message": "EMAIL_ERROR"}, status=400)
 
-            if re.match(passwd_regex, data["password"]) is None:
+            if not re.match(passwd_regex, passwd):
                 return JsonResponse({"message": "PASSWORD_ERROR"}, status=400)
-
-            if User.objects.filter(email=data['email']).exists():
-                return JsonResponse({"message" : "INVALID_USER"}, status=401) 
 
             User.objects.create(
                 name     = data["name"],
-                email    = data["email"],
-                passwd   = data["password"],
+                email    = email,
+                passwd   = passwd,
                 phone    = data["phone"],
                 )
             return JsonResponse({"message": "Success"}, status = 201)
         
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
-        except ValidationError:
-            return JsonResponse({"message": "EMAIL_ERROR"}, status=400)
+
 
 
