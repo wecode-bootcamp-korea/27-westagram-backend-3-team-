248e04c29d673b55e932ae1a0884d8accad20c33
lilo <eunehghim@gmail.com>
2021-11-22 16:34:26 +0900

    westagram_setting_sign_in_first

--- students/eunhyekim/users/views.py
+++ students/eunhyekim/users/views.py
@@ -4,7 +4,23 @@ from django.http      import JsonResponse
 from django.views     import View
 from users.models     import User
 
+class SignInView(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            password = User.objects.get(email = data["email"]).password
+            
+            if not data['password'] == password:
+                return JsonResponse({'message':'INVALID_USER'}, status = 400)
+            return JsonResponse({'message':'SUCCESS'},status = 200)
         
+        except User.DoesNotExist:
+            return JsonResponse({'message' : 'INVALID_USER'}, status = 401)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status = 400)
+
+
 class SignUpView(View):
     def post(self, request):
       
