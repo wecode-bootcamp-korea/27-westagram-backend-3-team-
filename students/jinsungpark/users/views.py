e5328f9a22adf4e4cdc4420328834d0c14c5f820
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2021-11-22 11:28:01 +0900

    회원가입 수정본9

--- students/jinsungpark/users/views.py
+++ students/jinsungpark/users/views.py
@@ -1,27 +1,25 @@
-from django.db            import IntegrityError
+from django.core.exceptions import ValidationError
 
-from django.http.response import JsonResponse
+from django.http.response   import JsonResponse
 
 import re
 import json
 
-from django.views         import View
-from .models              import User
+from django.views           import View
+from .models                import User
 
-
-# Create your views here.
 class SignUpView(View):
     def post(self, request):
         try:
-            data             = json.loads(request.body)
-            email_regex = '^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$'
+            data         = json.loads(request.body)
+            email_regex  = '^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$'
             passwd_regex = '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$'
 
             if re.match(email_regex, data["email"]) is None:
                 return JsonResponse({"message": "EMAIL_ERROR"}, status=400)
 
             if re.match(passwd_regex, data["password"]) is None:
-                return JsonResponse({"message": "PW_ERROR"}, status=400)
+                return JsonResponse({"message": "PASSWORD_ERROR"}, status=400)
 
             if User.objects.filter(email=data['email']).exists():
                 return JsonResponse({"message" : "INVALID_USER"}, status=401) 
@@ -33,9 +31,10 @@ class SignUpView(View):
                 phone    = data["phone"],
                 )
             return JsonResponse({"message": "Success"}, status = 201)
+        
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
-        except IntegrityError:
+        except ValidationError:
             return JsonResponse({"message": "EMAIL_ERROR"}, status=400)
 
 
