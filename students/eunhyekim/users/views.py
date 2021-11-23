b2e0f915858e7517d15f5a9e148a35f95930ddf5
lilo <eunehghim@gmail.com>
2021-11-23 11:23:46 +0900

    westagram_setting_sign_in_6th

--- students/eunhyekim/users/views.py
+++ students/eunhyekim/users/views.py
@@ -12,9 +12,8 @@ class SignInView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
-            user = User.objects.get(email = data["email"])
             
-            if user.password != data['password']:
+            if not User.objects.filter(email = data["email"], password = data["password"]).exists():
                 return JsonResponse({'message':'INVALID_USER'}, status = 401)
             return JsonResponse({'message':'SUCCESS'},status = 200)
         
@@ -38,14 +37,15 @@ class SignUpView(View):
             validation_email(email)
             validation_password(password)
             
+            if User.objects.filter(email = email).exists():
+                return JsonResponse({"MESSAGE":"User_Already_Exists"}, status = 400)
+
             User.objects.create(
                 name     = name,
                 email    = email,
                 password = password,
                 contact  = contact
             )
-            if User.objects.filter(email = email).exists():
-                return JsonResponse({"MESSAGE":"User_Already_Exists"}, status = 400)
 
             return JsonResponse({"MESSAGE":"SUCCESS"}, status = 201)
         
