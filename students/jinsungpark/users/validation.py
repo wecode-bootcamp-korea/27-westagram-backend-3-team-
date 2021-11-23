586659d5b36266993b71582b24a69934d0fb2cd0
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2021-11-23 10:31:05 +0900

    로그인 모듈 추가 수정본4

--- students/jinsungpark/users/validation.py
+++ students/jinsungpark/users/validation.py
@@ -4,10 +4,8 @@ from django.core.exceptions import ValidationError
 
 from .models                import User
 
-email_regex  = '^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$'
-passwd_regex = '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$'
-
 def Signup_email(email):
+    email_regex  = '^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$'
     if User.objects.filter(email=email).exists():
         raise ValidationError("EMAIL_ALREADY_EXISTS")
 
@@ -15,6 +13,7 @@ def Signup_email(email):
         raise ValidationError("EMAIL_ERROR")
 
 def Signup_password(password):
+    passwd_regex = '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$'
     if not re.match(passwd_regex, password):
         raise ValidationError("PASSWORD_ERROR")
 
