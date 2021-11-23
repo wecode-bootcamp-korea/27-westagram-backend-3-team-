5f7fd861f3827607685e627e0286d94dcd8a01ab
bakha1002 <yh795483@naver.com>
2021-11-23 10:56:55 +0900

    Update models.py #1

--- students/giuk/westagram/users/models.py
+++ students/giuk/westagram/users/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-# Create your models here.
 class User(models.Model):
     name         = models.CharField(max_length=45)
-    email        = models.EmailField()
-    password     = models.CharField(max_length=45)
+    email        = models.EmailField(max_length=100, unique=True)
+    password     = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=45)
 
     class Meta:
