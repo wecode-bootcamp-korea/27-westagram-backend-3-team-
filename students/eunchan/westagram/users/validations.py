e3928b1e90ea92218d16632781bed38e85678528
Daco <dacokim32@gmail.com>
2021-11-23 10:38:19 +0900

    정규표현식 함수내로 이동

--- students/eunchan/westagram/users/validations.py
+++ students/eunchan/westagram/users/validations.py
@@ -4,14 +4,13 @@ from django.core.exceptions import ValidationError
 
 from users.models           import Member
 
-email_regexp    = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-password_regexp = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&(){}\[\]])[A-Za-z\d@$!%*#?&(){}\[\]]{8,}$"
-
 def email_regexp_check(email):
+    email_regexp    = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
     if not re.match(email_regexp, email): 
         raise ValidationError("INVALID_EMAIL_ADDRESS")
 
 def password_regexp_check(password):
+    password_regexp = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&(){}\[\]])[A-Za-z\d@$!%*#?&(){}\[\]]{8,}$"
     if not re.match(password_regexp , password):
         raise ValidationError("INVALID_PASSWORD")
 
