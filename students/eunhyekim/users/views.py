b3e2b51229d62002a63277e030f3e473fefc3179
lilo <eunehghim@gmail.com>
2021-11-24 01:24:58 +0900

    westagram_authorized_first

--- students/eunhyekim/users/views.py
+++ students/eunhyekim/users/views.py
@@ -1,4 +1,4 @@
-import json, bcrypt
+import json, bcrypt, jwt
 
 from django.http                import JsonResponse
 from django.views               import View
@@ -11,14 +11,21 @@ from users.validation           import validation_email, validation_password
 class SignInView(View):
     def post(self, request):
         try:
-            data = json.loads(request.body)
-            
-            if not User.objects.filter(email = data["email"], password = data["password"]).exists():
-                return JsonResponse({'message':'INVALID_USER'}, status = 401)
-            return JsonResponse({'message':'SUCCESS'},status = 200)
+            data     = json.loads(request.body)
+            password = User.objects.get(email = data["email"]).password 
+            user_id  = User.objects.get(email = data["email"]).id 
+        
+            if bcrypt.checkpw(data['password'].encode('utf-8'), password.encode('utf-8')) == True:
+                access_token = jwt.encode({'id':user_id}, 'secret', algorithm = 'HS256')
+          
+                return JsonResponse({'TOKEN': access_token}, status = 200)
+            return JsonResponse({'MESSAGE':"UNAUTHORIZED"}, status = 401) 
+
+        except User.DoesNotExist:
+            return JsonResponse({'MESSAGE':'INVALID_USER'}, status = 401)
 
         except KeyError:
-            return JsonResponse({'message':'KEY_ERROR'}, status = 400)
+            return JsonResponse({'MESSAGE' :'KEY_ERROR'}, status = 400)
 
 
 class SignUpView(View):
