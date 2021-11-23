f2f1237d7977d6dc5bf4b5a650926b6339abd90d
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2021-11-23 17:19:30 +0900

    wip

--- students/jinsungpark/users/validation.py
+++ students/jinsungpark/users/validation.py
@@ -2,15 +2,15 @@ import re
 
 from django.core.exceptions import ValidationError
 
-def Signup_email(email):
+def signup_email(email):
     email_regex  = '^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$'
 
     if not re.match(email_regex, email):
         raise ValidationError("EMAIL_ERROR")
 
-def Signup_password(password):
+def signup_password(password):
     passwd_regex = '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$'
-    
+
     if not re.match(passwd_regex, password):
         raise ValidationError("PASSWORD_ERROR")
 
